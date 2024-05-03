90057a823802c268be29801514d5b8e6995f594c
Chamoli <godchamoli@gmail.com>
2024-05-03 20:24:06 +0530

    fix: Correct typo in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Let's Meet
+          Let&apos;s Meet
         </p>
       </Link>
       <div className="flex-between gap-5">
